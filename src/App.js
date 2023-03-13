b6a1ccf44535023a842fc394defc0b3dd851b832
Franco De Leon <francodeleon97@hotmail.com>
2023-03-12 18:14:54 -0600

    show home sections conditionally

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MatcherPage from "./pages/MatcherPage";
 
 function App() {
   const [signer, setSigner] = useState(null);
+  const [user, setUser] = useState(null);
 
   const connectWallet = async () => {
     const _signer = await getSigner();
@@ -14,25 +15,27 @@ function App() {
 
   return (
     <div className="relative app">
-      <AppHeader />
+      {user && <AppHeader />}
       <section className="body px-4 py-2">
-        <div className="p-4">
-          <div className="text-center">
-            <img className="logo" src="/metch-logo.png" alt="Metch Logo" />
-            <p className="text-purple-500 mb-2">
-              Match with other frens and start hacking
-            </p>
-            <button
-              onClick={connectWallet}
-              className="px-4 py-2 border-2 border-purple-500 rounded-lg text-purple-500"
-            >
-              Connect Wallet
-            </button>
+        {!signer && (
+          <div className="p-4">
+            <div className="text-center">
+              <img className="mx-auto" src="/metch-logo.png" alt="Metch Logo" />
+              <p className="text-purple-500 mb-2">
+                Match with other frens and start hacking
+              </p>
+              <button
+                onClick={connectWallet}
+                className="px-4 py-2 border-2 border-purple-500 rounded-lg text-purple-500"
+              >
+                Connect Wallet
+              </button>
+            </div>
           </div>
-        </div>
+        )}
         {signer && <MatcherPage />}
       </section>
-      <FooterMenu />
+      {user && <FooterMenu />}
     </div>
   );
 }
